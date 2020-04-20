7a3afe09244f7e5cb394ebab44324fc87d276a8d
Caleb Cowen <calebcowen@gmail.com>
2020-04-20 15:19:03 -0500

    add seeds to the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+MenuItem.create([
+  { name: 'TestItem', price: 10.99, description: 'The first test item' },
+  { name: 'TestItem2', price: 11.99, description: 'The second test item'  }
+])
